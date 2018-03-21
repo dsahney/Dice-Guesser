62968fb5664a6d319e7a73cbc1652b06219a6940
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-03-21 09:59:31 -0400

    Update Dice_guesser.py

--- Dice_guesser.py
+++ Dice_guesser.py
@@ -1,17 +1,13 @@
-begin_question = 'Would you like to play?\n Answer yes or no.'
-start_answer = input(begin_question)
-
-def dice_game(start_answer):
-    if start_answer == 'yes':
-        import random
-        dice_result = random.randint(1, 6)
-        question1 = 'Hello, please guess what number has been rolled by the die.\n Note that a die has six sides and therefore can land on either side 1, 2, 3, 4, 5, or 6.'
-        tries = 0
-        while int(input(question1)) != dice_result:
-            print('Please try again.')
-            tries += 1
-            if int(input(question1)) == dice_result:
-                print('Congratulations! You have correctly guessed the side the die has landed on.\n It took you', tries, 'tries to guess the correct answer.')
-
-
-
+import random
+num = random.randint(1,6)
+guess = input('Please guess a number between 1 and 6')
+int_guess = int(guess)
+if num == int_guess:
+    print('You are correct!')
+while num != int_guess:
+    int_guess = int(input('Please guess a number between 1 and 6'))
+    if int_guess == num:
+        print('You are correct!')
+        break
+    else:
+        print('Please try again')
